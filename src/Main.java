1de830749e0a2d5af9008216fedea6f55dfb4c52
LexHeadko <n-odes11rus@yandex.ru>
2023-06-20 16:52:25 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        //task2();
+        //task3();
+        //task4();
+        //task5();
+        //task6();
+    }
+
+    public static void task1() {
+        int p = 2;
+        if (p % 2 == 1 || p == 0) {
+            System.out.println("p - нечетное число!");
+        }else {
+            System.out.println("p - четное число!");
+        }
+    }
+}
